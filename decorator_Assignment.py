4e93fd36dcaa4be5047822129e52469477c38591
Akbar2424 <aktamboli29@gmail.com>
2023-07-07 15:43:42 +0530

    Commited final code

---
+++ decorator_Assignment.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def div(x,y):
+    return x/y
+
+def mult(x,y):
+    return x*y
+
+def result(func,x,y):
+    return func(x,y)
+
+final=result(add,20,5)
+print(final)
